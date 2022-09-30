7286d19f8cce11bf4fe3f5af019d36ecc199ff64
franckee111 <frestrada.dwfsc9@ucamp.utel.edu.mx>
2022-09-30 15:36:52 -0500

    Graficar con Axios y Fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,65 @@ const base_api = "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=";
 const key_api = "DV6KDKvpInnMAW075R5HSZtSbaa1uwme7T115NRR";
 const url_api = `${base_api}${key_api}`;
 const tblNasa = document.getElementById('tblNasa');
-const posicion = 0;
+const position = 0;
 const graph_velocity = document.getElementById('graph_velocity').getContext('2d');
 const graph_orbiting = document.getElementById('graph_orbiting').getContext('2d');
-const loaderContainer =document.getElementById ("loaderContainer");
-const mainContainer   =document.getElementById("mainContainer");
-const secondContainer =document.getElementById("secondContainer") 
+const loaderContainer = document.getElementById("loaderContainer");
+const mainContainer = document.getElementById("mainContainer");
+const secondContainer = document.getElementById("secondContainer");
+
+const relativeVelocity = async () => {
+    try {
+        const respuesta = await axios.get(url_api);
+        // console.log(respuesta.data);
+
+        let labels_asteroide = respuesta.data.near_earth_objects.map((item) => {
+            return item.name_limited;
+        });
+
+        let data_velocity = respuesta.data.near_earth_objects.map((item) => {
+            return parseFloat(item.close_approach_data[position].relative_velocity.kilometers_per_second).toFixed(2);
+        });
+
+        const velocity = new Chart(graph_velocity, {
+            type: 'bar',
+            data: {
+                labels: labels_asteroide,
+                datasets: [{
+                    label: 'Velocidad Relativa',
+                    data: data_velocity,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
 
 function cargarDatos() {
     fetch(url_api, { method: "GET" })
@@ -15,16 +68,11 @@ function cargarDatos() {
         .then((result) => {
             tblNasa.innerHTML = "";
 
-            let labels_asteroide = result.near_earth_objects.map((item) => {
-                return item.name_limited.toUpperCase();
-            });
-
-            let data_velocity = result.near_earth_objects.map((item) => {
-                return parseFloat(item.close_approach_data[posicion].relative_velocity.kilometers_per_second).toFixed(2);
-            });
+            //Asteroids orbiting in:
+            let array_labels = ['Earth', 'Juptr', 'Mars', 'Other'];
 
             let data_orbiting = result.near_earth_objects.map((item) => {
-                return item.close_approach_data[posicion].orbiting_body;
+                return item.close_approach_data[position].orbiting_body;
             });
 
             let earth = 0;
@@ -33,7 +81,7 @@ function cargarDatos() {
             let other = 0;
             const array_orbiting = [];
 
-            for(let i=0; i<data_orbiting.length; i++){
+            for (let i = 0; i < data_orbiting.length; i++) {
                 switch (data_orbiting[i]) {
                     case 'Earth': earth++;
                         break;
@@ -44,45 +92,8 @@ function cargarDatos() {
                     default: other++;
                 }
             }
-            //Asteroids orbiting in:
-            let array_labels = ['Earth', 'Juptr', 'Mars','Other'];
-            
-            array_orbiting.push(earth,juptr,mars,other);
 
-            const velocity = new Chart(graph_velocity, {
-                type: 'bar',
-                data: {
-                    labels: labels_asteroide,
-                    datasets: [{
-                        label: 'Velocidad Relativa',
-                        data: data_velocity,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 2
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
-            });
+            array_orbiting.push(earth, juptr, mars, other);
 
             const orbiting = new Chart(graph_orbiting, {
                 type: 'doughnut',
@@ -121,9 +132,9 @@ function cargarDatos() {
                 <td> ${asteroide.name} </td>
                 <td> ${asteroide.estimated_diameter.kilometers.estimated_diameter_min.toFixed(2)} min - ${asteroide.estimated_diameter.kilometers.estimated_diameter_max.toFixed(2)} max </td>
                 <td> ${asteroide.is_potentially_hazardous_asteroid}  </td>
-                <td> ${parseFloat(asteroide.close_approach_data[posicion].relative_velocity.kilometers_per_second).toFixed(2)}  </td>
+                <td> ${parseFloat(asteroide.close_approach_data[position].relative_velocity.kilometers_per_second).toFixed(2)}  </td>
                 <td> ${(new Date(Date.parse(asteroide.orbital_data.first_observation_date))).toLocaleDateString()}  </td>
-                <td> ${asteroide.close_approach_data[posicion].orbiting_body}  </td>
+                <td> ${asteroide.close_approach_data[position].orbiting_body}  </td>
             </tr>`;
                 tblNasa.innerHTML += tr;
             }
@@ -138,4 +149,5 @@ function cargarDatos() {
         .catch((error) => console.log(error));
 }
 
-cargarDatos();
+relativeVelocity(); //axios sync await
+cargarDatos(); //fetch promise
